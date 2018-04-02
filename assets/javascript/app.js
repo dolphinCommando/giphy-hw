7145a9033f5b91b32f792f4876256a5f56ce3120
Matthew Buchanan <matt@Matthews-MacBook-Air.local>
2018-04-02 18:31:41 -0400

    custom buttons are now saved in session storage; bugfix for active topics on click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
-    var topics = ['Superman', 'Batman', 'Wonder Woman', 'Teen Titans'];
     var activeButton;
     var activeText = '';
     
     $('body').on('click','.gif-button', function() {
         //Removes button
-        
-        if ($(this).attr('class').includes('bg-danger')) {
-            if($(this).text() === activeText) {
-                console.log('Removing active button');
+        const that = $(this);
+        if (that.attr('class').includes('bg-danger')) {
+            if(that.text() === activeText) {
+                //console.log('Removing active button');
                 $('#gif-gifs .row').empty();
-                 $('#help').css('visibility', 'visible');
+                $('#help').css('visibility', 'visible');
             }
-            $(this).remove();
-            var textToDelete = $(this).text();
-            topics = topics.filter(function(topic) {
+            that.remove();
+            var textToDelete = that.text();
+            var arr = (JSON.parse(window.sessionStorage.topics)).filter(function(topic) {
                 return topic !== textToDelete;
-            })
+            });
+            window.sessionStorage.topics = JSON.stringify(arr);
             updateButtonBadge();
         } else {
 
         //Renders the GIF page
-        var queryText = $(this).text();
+        var queryText = that.text();
         queryText = queryText.trim();
         queryText = queryText.replace(' ', '+');
         //console.log(queryText);
         const queryURL = `https://api.giphy.com/v1/gifs/search?q=${queryText}&api_key=1J5qzsdvlKkaRytoNtQL0GuD38BGTQM3&limit=10`;
-        console.log(queryURL);
-        if (!($(this).attr('class').includes('active'))) {
+        //console.log(queryURL);
+        if (!(that.attr('class').includes('active'))) {
         $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-            console.log(response);
+            //console.log(response);
             $('#gif-gifs .row').empty();
             response.data.forEach(function(element) {
                 $('#gif-gifs .row').append(`
@@ -51,42 +51,50 @@ $(document).ready(function() {
         if(typeof activeButton !== 'undefined') {
             activeButton.attr('class', 'dropdown-item gif-button');
         }
-        $(this).attr('class', 'dropdown-item gif-button active');
-        activeButton = $(this);
-        activeText = $(this).text();
+        that.attr('class', 'dropdown-item gif-button active');
+        activeButton = that;
+        activeText = that.text();
        
 
         //Clicking active button twice turns it off
         } else {
-            $(this).attr('class', 'dropdown-item gif-button');
+            that.attr('class', 'dropdown-item gif-button');
             $('#gif-gifs .row').empty();
             $('#help').css('visibility', 'visible');
+            activeText = '';
 
         }
+
+
         }
     }); 
 
     $('body').on('click', '#add-submit', function(event) {
         event.preventDefault();
-        console.log($('#add-text'));
-        topics.push($('#add-text').val());
+        //console.log($('#add-text'));
+        var arr = JSON.parse(window.sessionStorage.topics);
+        arr.push($('#add-text').val());
+        window.sessionStorage.topics = JSON.stringify(arr);
         renderButtons();
         updateButtonBadge();
-         $('#add-text').val(''); 
+        $('#add-text').val(''); 
        
     });
 
     $('body').on('click', '#edit-buttons', function(event) {
         event.preventDefault();
         if ($(this).attr('class').includes('active')) {
-            console.log('Active text is ' + activeText);
-            $('.gif-button[class!="dropdown-item gif-button active"]').attr('class','dropdown-item gif-button');
+            //console.log('Active text is ' + activeText);
+            $('.gif-button').attr('class', function(val) {
+                //console.log($(this));
+                return ($(this).text() === activeText) ? 'dropdown-item gif-button active' : 'dropdown-item gif-button';
+            });
             $(this).text('Edit buttons');
             $(this).attr('class', 'dropdown-item');
-            $(`.gif-button:contains(${activeText})`).attr('class', 'dropdown-item gif-button active');
-            
+            //console.log('typeof $(".gif-button") ' + $('.gif-button'));
         } else {
-        console.log('Click button to remove');
+        //console.log('Click button to remove');
+        //console.log($(this));
         $(this).attr('class', 'dropdown-item active');
         $('body').find('.gif-button').attr('class', 'dropdown-item gif-button bg-danger text-white');
         $(this).text('Cancel');
@@ -95,7 +103,7 @@ $(document).ready(function() {
 
     $('body').on('click', '.gif', function(event) {
         event.preventDefault();
-        console.log('Clicked ' + $(this).attr('alt'));
+        //console.log('Clicked ' + $(this).attr('alt'));
         var state = $(this).attr('data-state');
         if (state === 'animated') {
             $(this).attr('src', `${$(this).attr('data-still')}`);
@@ -104,14 +112,18 @@ $(document).ready(function() {
             $(this).attr('src', `${$(this).attr('data-animated')}`);
             $(this).attr('data-state', 'animated');
         }
-
     });
 
     //Renders buttons in the 'Topics' dropdown menu
     function renderButtons() {
-        console.log(topics);
+        //console.log(topics);
+        //console.log(typeof window.sessionStorage.topics);
+        if (typeof window.sessionStorage.topics === 'undefined') { 
+            window.sessionStorage.clear();
+            window.sessionStorage.topics = JSON.stringify(['Superman', 'Batman', 'Wonder Woman', 'Teen Titans']);
+        }
         $('#gif-buttons').empty();
-        topics.forEach(function(topic) {
+        (JSON.parse(window.sessionStorage.topics)).forEach(function(topic) {
             $('#gif-buttons').append(`<button class="dropdown-item gif-button" type="button">${topic}</button>`);
         }); 
         $('#gif-buttons').append('<div class="dropdown-divider"></div>');
@@ -120,17 +132,12 @@ $(document).ready(function() {
     }
 
     function updateButtonBadge() {
-        $('.topic-number').text(`${topics.length}`);
+        var arr = JSON.parse(window.sessionStorage.topics);
+        //console.log('updateButtonBadge JSON parse ' + arr);
+        $('.topic-number').text(`${arr.length}`);
     }
-
 
      renderButtons();
      updateButtonBadge(); 
-
-
-    
-
-
-
     
 });
